bc3b6afee33a33031447db3f553b146b7cc2a13c
seungdobaek <seungdo100@gmail.com>
2025-09-19 18:01:31 +0900

    25_09_19 데이터구조 나머지(튜플,딕셔너리,셋)

--- source/01_python/lst.py
+++ source/01_python/lst.py
@@ -1,10 +1,9 @@
-def safe_index(lst, item):
+def safe_index(lst, item, start=0):
     '''
-    첫번째 매개변수 lst에서 item 요소가 있는 index를 반환.
+    첫번째 매개변수 lst[start:]에서 item 요소가 있는 index를 반환.
     item 요소가 없으면 -1을 반환
-    lst: 요소를 찾고 싶은 리스트
+    lst: 요소를 찾고 싶은 iterable(리스트, 튜플)
     item: 찾고 싶은 값
+    start: 서치를 시작할 index(기본값: 0)
     '''
-    if item in lst:
-        return(lst.index(item))
-    return -1
+    return lst.index(item,start) if (item in lst[start:]) else -1
